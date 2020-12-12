f49deaeeeb1b0149f6132a0457164fdf8d64c3f1
Devair Augusto da Silva <53318711+DevairS@users.noreply.github.com>
2020-12-12 19:11:33 -0300

    Create Interface IEstadoDao

--- dao/interfaces/ICategoriaDao.php
+++ dao/interfaces/ICategoriaDao.php
@@ -1,8 +1,8 @@
 <?php
     interface ICategoriaDao
     {
-        public function create(Categoria $Categoria): void; //Cria um Categoria
-        public function update(Categoria $Categoria):void; //Atualiza um Categoria
+        public function create(Categoria $categoria): void; //Cria um Categoria
+        public function update(Categoria $categoria):void; //Atualiza um Categoria
         public function delete(int $id):void; //Deleta um Categoria
         public function listAll(); //Lista todos os Categoria
         public function showSingleCategoria(int $id); //lista um Categoria
